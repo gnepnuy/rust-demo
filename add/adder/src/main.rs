ff34e34ed3521a9d457f8d0b2ba5dab7569c42d3
yunpeng <zhuyunpeng@aliyun.com>
2024-03-23 14:29:17 +0800

    202403231429

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,7 +1,10 @@
 use add_one as aa;
-
+use add_two as bb;
 fn main() {
 
     let num = 10;
     println!("Hello, world! {} plus one is {}!",num,aa::add_one(num));
+    println!("Hello, world! {} plus two is {}!",num,bb::add_two(num));
+
+
 }
--- add/target/debug/adder.d
+++ add/target/debug/adder.d
@@ -1,1 +1,1 @@
-E:\workspace\rust-demo\add\target\debug\adder.exe: E:\workspace\rust-demo\add\add_one\src\lib.rs E:\workspace\rust-demo\add\adder\src\main.rs
+E:\workspace\rust-demo\add\target\debug\adder.exe: E:\workspace\rust-demo\add\add_one\src\lib.rs E:\workspace\rust-demo\add\add_two\src\lib.rs E:\workspace\rust-demo\add\adder\src\main.rs
