5015835211da081c13ccf7e6a8c18e50761c6733
yunpeng <zhuyunpeng@aliyun.com>
2024-04-18 09:02:25 +0800

    update

---
+++ advanced_trait/target/debug/advanced_trait.d
@@ -0,0 +1,1 @@
+/Users/yunpeng/workspase/rust-demo/advanced_trait/target/debug/advanced_trait: /Users/yunpeng/workspase/rust-demo/advanced_trait/src/main.rs
---
+++ advanced_trait/target/debug/deps/advanced_trait-c6c63db1c4b8dce3.d
@@ -0,0 +1,5 @@
+/Users/yunpeng/workspase/rust-demo/advanced_trait/target/debug/deps/advanced_trait-c6c63db1c4b8dce3: src/main.rs
+
+/Users/yunpeng/workspase/rust-demo/advanced_trait/target/debug/deps/advanced_trait-c6c63db1c4b8dce3.d: src/main.rs
+
+src/main.rs:
